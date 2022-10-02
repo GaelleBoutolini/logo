521bc8a8b8c49b69cc46e1481ed56d4dcd78e3bd
hannazoe <linegaelle2000@yahoo.fr>
2022-10-02 23:06:23 +0200

    Add +JS + file

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let myHeading = document.querySelector('h2');
+myHeading.textContent = ' Venez découvrir la cuisine de Bénie...Et mettez vos papilles en joie. Découvrez votre menu du jour.';
+/*
+https://developer.mozilla.org/fr/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+"site pour apprendre les bases de Javascript"
+*/ 
